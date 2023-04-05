dc2bd12a108c81163eba43f1d1e77427b35e0f34
Jarek B <jaroslaw.bielecki@poczta.fm>
2023-04-05 14:49:08 +0200

    end

---
+++ js/slicksettings.js
@@ -0,0 +1,17 @@
+$(".team-carousel").slick({
+	arrows: false,
+	autoplay: true,
+	mobileFirst: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 554,
+			settings: { slidesToShow: 2 },
+		},
+		{
+			breakpoint: 992,
+			settings: { slidesToShow: 3 },
+		},
+	],
+});
